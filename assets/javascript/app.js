0fd85bcab5c913657bf049e2cb6575b2037d8d71
Unknown <jasminepkim@gmail.com>
2017-08-23 15:13:41 -0700

    Draft on 8/23 @ 3:13pm
    Before attempting to move q's over from HTML to JS and creating hide and show functions in JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,50 @@
 // VARIABLES
-var hideStart;
+var startPage;
+var mainPage;
+var endPage;
 var showClock;
 var correct;
 var incorrect;
 var unanswered;
 
+// ***MOVE HTML QUESTIONS OVER TO JS!***
+
+// The questions and DONE button are hidden until the user clicks the START button
+startPage = $(".container").hide();
+startPage = $("#donebtn").hide();
+endPage = $(".results").hide();
+
+// Buttons for the user to use to begin and end the game
 $("#startbtn").click(startGame);
+$("#donebtn").click(seeResults);
+
+// ***CREATE "HIDE" AND "SHOW" FUNCTIONS AND CALL THEM INSTEAD!!!***
 
 // FUNCTIONS
 function startGame() {
-    hideStart = $("#startbtn").hide();
-    // showClock = $("#startbtn").text("<p>You have" + "..." + "left</p>");
+    mainPage = $("#startbtn").hide();
+    mainPage = $(".container").show();
+    mainPage = $("#donebtn").show();
+    showClock = $(".startbutton").text("You have ... left!");
+    endPage = $(".results").hide();
     // hideStart = $("#startbtn").hide(1000, startGame);
     // currently stuck in an infinite loop when refreshing AFTER start button has been pressed....
 }
+
+// When the user is ready to submit their answers:
+function seeResults() {
+    endPage = $(".container").hide();
+    endPage = $("#donebtn").hide();
+    endPage = $(".startbutton").hide();
+    endPage = $(".results").show();
+}
+
+// function hideStuff() {
+//     startPage = $(".container").hide();
+//     startPage = $("#donebtn").hide();
+//     mainPage = $("#startbtn").hide();
+//     endPage = $(".header2").hide();
+// }
 
 // needs to show time counter to start the game
 
